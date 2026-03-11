7ebcd6260b6a6816a7abc590a45b08267447c693
JpStar06 <henriqueeapahel@gmail.com>
2026-03-11 13:03:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import discord
 from discord.ext import commands
-import json
 import asyncio
+import os
+import dotenv
+
+dotenv.load_dotenv()
+TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-with open("config.json", "r") as f:
-    config = json.load(f)
-
 @bot.event
 async def on_ready():
     await bot.tree.sync()
     print(f"Logado como {bot.user}")
 
+
 async def main():
     async with bot:
         await bot.load_extension("cogs.comandos")
         await bot.load_extension("cogs.tickets")
-        await bot.start(config["token"])
+        await bot.start(TOKEN)
+
 
 asyncio.run(main())
