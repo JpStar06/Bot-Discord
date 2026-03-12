8b3ea45c05ef91a42854d6df4c2ec58bdd17a2c5
JpStar06 <henriqueeapahel@gmail.com>
2026-03-12 12:53:44 -0300

    refaturçao da estrutura part 1

--- cogs/fun/8ball.py
+++ cogs/fun/8ball.py
@@ -4,11 +4,10 @@ from discord.ext import commands
 import random
 
 
-class Misc(commands.Cog):
+class ball(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-
+    
     @app_commands.command(name="8ball", description="Faça uma pergunta para a bola mágica.")
     async def eightball(self, interaction: discord.Interaction, pergunta: str):
 
@@ -32,20 +31,5 @@ class Misc(commands.Cog):
 
         await interaction.response.send_message(embed=embed)
 
-
-    @app_commands.command(name="fight", description="Lute com outro usuário.")
-    async def fight(self, interaction: discord.Interaction, usuario: discord.Member):
-
-        vencedor = random.choice([interaction.user, usuario])
-
-        embed = discord.Embed(
-            title="⚔️ Luta!",
-            description=f"{interaction.user.mention} lutou contra {usuario.mention}!\n\n🏆 **Vencedor:** {vencedor.mention}",
-            color=0xe74c3c
-        )
-
-        await interaction.response.send_message(embed=embed)
-
-
 async def setup(bot):
-    await bot.add_cog(Misc(bot))
+    await bot.add_cog(ball(bot))
--- main.py
+++ main.py
@@ -1,33 +1,31 @@
 import discord
 from discord.ext import commands
-import asyncio
 import os
-import dotenv
-from database import setup_database
 
-dotenv.load_dotenv()
-TOKEN = os.getenv("TOKEN")
+intents = discord.Intents.all()
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
 
 @bot.event
 async def on_ready():
     await bot.tree.sync()
     print(f"Logado como {bot.user}")
-    setup_database()
-    print("banco de dados conectado")
-    await bot.tree.sync()
+
+async def load_cogs():
+    for root, dirs, files in os.walk("./cogs"):
+        for file in files:
+            if file.endswith(".py"):
+                path = os.path.join(root, file)
+                module = path.replace("/", ".").replace("\\", ".")[:-3]
+                await bot.load_extension(module)
 
 async def main():
     async with bot:
-        await bot.load_extension("cogs.comandos")
-        await bot.load_extension("cogs.tickets")
-        await bot.load_extension("cogs.comandos_mod")
-        await bot.load_extension("cogs.Misc")
-        await bot.start(TOKEN)
+        await load_cogs()
+        await bot.start("TOKEN")
 
-
+import asyncio
 asyncio.run(main())
