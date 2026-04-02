f65a0daf4665b38d9a2e44258c1111e8f07e0d63
JpStar06 <henriqueeapahel@gmail.com>
2026-04-02 10:24:20 -0300

    Update tickets.py

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -156,28 +156,51 @@ class Tickets(commands.Cog):
     #---------------------EDITAR PAINEL---------------------
     @tickets.command(name="editar-painel", description="Edita o painel principal do ticket")
     @app_commands.checks.has_permissions(administrator=True)
-    async def editarticket(self, interaction: discord.Interaction, id: int, novo_titulo: str = None, novo_descricao: str = None, nova_cor: int = None, imagem_url: str = None):
-        async with self.pool.acquire() as conn:
-            ticket_data = await conn.fetchrow("SELECT title, description, color, image FROM tickets WHERE id=$1, guild_id=$2", id, interaction.guild.id)
+    async def editarticket(
+        self,
+        interaction: discord.Interaction,
+        id: int,
+        novo_titulo: str = None,
+        novo_descricao: str = None,
+        nova_cor: int = None,
+        imagem_url: str = None
+    ):
+
+        conn = await get_connection()
+
+        ticket_data = await conn.fetchrow(
+            "SELECT titulo, descricao, cor, imagem FROM tickets WHERE id=$1 AND guild_id=$2",
+            id,
+            interaction.guild.id
+        )
+
         if not ticket_data:
-            await interaction.reponse.send_message("Ticket não encontrado.", ephemeral=True)
+            await interaction.response.send_message("Ticket não encontrado.", ephemeral=True)
+            await conn.close()
             return
-        
-        title = novo_titulo or ticket_data["title"]
-        description = novo_descricao or ticket_data["description"]
-        color = nova_cor or ticket_data["color"]
-        image = imagem_url or ticket_data["image"]
 
-        await conn.execute("""
-            UPDATE tickets SET title=$1, description=$2, color=$3, image=$4
+        titulo = novo_titulo or ticket_data["titulo"]
+        descricao = novo_descricao or ticket_data["descricao"]
+        cor = nova_cor or ticket_data["cor"]
+        imagem = imagem_url or ticket_data["imagem"]
+
+        await conn.execute(
+            """
+            UPDATE tickets 
+            SET titulo=$1, descricao=$2, cor=$3, imagem=$4
             WHERE id=$5 AND guild_id=$6
-        """, title, description, color, image, id, interaction.guild_id)
+            """,
+            titulo, descricao, cor, imagem, id, interaction.guild.id
+        )
 
-        embed = discord.Embed(title=title, description=description, color=color)
-        if image:
-            embed.set_image(url=image)
-        await interaction.response.send_message("Embed atualizado:", embed=embed)
+        await conn.close()
 
+        embed = discord.Embed(title=titulo, description=descricao, color=cor)
+
+        if imagem:
+            embed.set_image(url=imagem)
+
+        await interaction.response.send_message("✅ Embed atualizado:", embed=embed)
     #---------------------DELETAR--------------------
     @tickets.command(name="deletar", description="Deleta um ticket.")
     @app_commands.checks.has_permissions(administrator=True)
