d398f08018760db4c8c034ebac08c3c2d20e7b1d
JpStar06 <henriqueeapahel@gmail.com>
2026-04-23 11:34:55 -0300

    preparando o setup

--- cogs/setup/cogs.py
+++ cogs/setup/cogs.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+from . import embeds
 
 
 class SetupCog(commands.Cog):
@@ -38,60 +39,9 @@ class SetupCog(commands.Cog):
         except:
             pass
 
-        # 📘 EMBED 1
-        embed1 = discord.Embed(
-            title="🎫 Sistema de Tickets",
-            description="Aprenda a usar o sistema de tickets.",
-            color=discord.Color.blue()
-        )
-
-        embed1.add_field(
-            name="🛠 Criar ticket",
-            value="Use `/tickets criar`",
-            inline=False
-        )
-
-        embed1.add_field(
-            name="✏️ Editar ticket",
-            value="Use `/tickets editar <id>`",
-            inline=False
-        )
-
-        embed1.add_field(
-            name="📤 Enviar ticket",
-            value="Use `/tickets enviar <id_tickets><id_canal>`",
-            inline=False
-        )
-
-        # 📘 EMBED 2
-        embed2 = discord.Embed(
-            title="🎛 Editor de Ticket",
-            description="Use os botões para configurar:",
-            color=discord.Color.green()
-        )
-
-        embed2.add_field(name="✏️ Título", value="Define o título", inline=True)
-        embed2.add_field(name="📝 Descrição", value="Define o texto", inline=True)
-        embed2.add_field(name="🎨 Cor", value="Cor do embed", inline=True)
-        embed2.add_field(name="🖼️ Imagem", value="Imagem do ticket", inline=True)
-        embed2.add_field(name="👮 Atendente", value="Cargo staff", inline=True)
-
-        # 📘 EMBED 3
-        embed3 = discord.Embed(
-            title="🎫 Como funciona",
-            description=(
-                "1. Clique no botão 🎫\n"
-                "2. Canal privado será criado\n"
-                "3. Staff será notificado\n"
-                "4. Use ❌ para fechar"
-            ),
-            color=discord.Color.red()
-        )
 
         # 📩 enviar
-        await channel.send(embed=embed1)
-        await channel.send(embed=embed2)
-        await channel.send(embed=embed3)
+        await channel.send(embed=embeds.embed1())
 
         await interaction.followup.send(
             f"✅ Setup concluído em {channel.mention}",
---
+++ cogs/setup/embeds.py
@@ -0,0 +1,10 @@
+import discord
+def embed1():
+    embed = discord.Embed(
+        title="📌 Bem-vindo ao tutorial da Aiko!",
+        description=(
+            "Aqui vou te mostrar como usar o sistema de tickets, embeds e economia da Aiko!\nPrometo que tudo será bem simples e fácil de entender, então vamos lá! :3"
+        ),
+        color=discord.Color.pink()
+    )
+    return embed
